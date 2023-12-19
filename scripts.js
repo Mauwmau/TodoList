69db5897d431d0247d6ca39c0274309797ec8aa2
Maurilio Meireles <maurilio.meireles@gmail.com>
2023-12-18 23:34:49 -0300

    todolist objects can be created, deleted and displayed

--- scripts.js
+++ scripts.js
@@ -1,15 +1,31 @@
 let todolist = [] // Declaration of a list, will store the to-do's
 
+let counter = 0
 // How a to-do item should look like
-const todoitem = {
-    checked: false,
-    description: "Name of a chore"
+// const todoitem = {
+//     id: 0, // Number
+//     checked: false, // Boolean
+//     description: "Name of a chore" // String
+// }
+
+function createTodoItem(desc) {
+    const newitem = {
+        id: todolist.length + 1 + counter,
+        checked: false,
+        description: desc
+    }
+
+    console.log(desc, newitem)
+
+    todolist.push(newitem)
 }
 
-newitem = todoitem;
-newitem.checked = true
-console.log(newitem)
+function deleteTodoItem(itemId) {
+    todolist = todolist.filter((item)=>item.id !== itemId)
+    counter = counter + 1
+}
 
+// Gets the text field and returns the string written by the user
 function readUserInput() {
     const userInput = document.getElementById("chorename")
     const userStr = userInput.value
@@ -17,21 +33,26 @@ function readUserInput() {
     return userStr
 }
 
+// Reads user task and adds to the list, activated when the form is submited
 function handleSubmit(){
-    const chorename = readUserInput()
-    addToDo(chorename)
+    const taskname = readUserInput()
+    // addToDo(taskname)
+    createTodoItem(taskname)
+    console.log(todolist);
 }
 
+// Creates the elements that will be added to the DOM.
+// params:
+//  - itemname: string that describes the task
 function addToDo(itemname) {
     if(itemname != null && itemname != ""){
         let newitembox = document.createElement("INPUT")
         newitembox.setAttribute("type", "checkbox")
         newitembox.setAttribute("value", false)
+        newitembox.addEventListener("input", () => {removeTodo(itemname)})
 
-        let newitemname = document.createElement("INPUT")
-        newitemname.setAttribute("type", "text")
-        newitemname.setAttribute("value", itemname)
-        newitemname.setAttribute("readonly", true)
+        let newitemname = document.createElement("P")
+        newitemname.innerHTML = itemname
 
         let licontainer = document.createElement("LI")
         licontainer.appendChild(newitembox)
@@ -42,3 +63,12 @@ function addToDo(itemname) {
     }
 }
 
+function removeTodo(param) {
+    alert("Remover " + param)
+}
+
+function receiveList() {
+    todolist.forEach((item)=>{
+        addToDo(item.description)
+    })
+}
